89986613055623b689a0442db2d94962bbfdf843
yekyung2 <skybluee2014@gmail.com>
2021-02-25 15:19:42 +0900

    add css cards view

--- techalook/models.py
+++ techalook/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
-
-# Create your models here.
 class Techblog(models.Model):
     name = models.CharField(max_length=150)
     create_date = models.DateTimeField(auto_now=False, auto_now_add=False)
@@ -17,7 +14,9 @@ class Card(models.Model):
     url = models.URLField(max_length=500, default="")
     img = models.URLField(max_length=500, blank=True)
     date = models.DateField()
-    directory = models.ForeignKey(Techblog, on_delete=models.CASCADE, default="")
+    directory = models.ForeignKey(
+        Techblog, related_name="cards", on_delete=models.CASCADE, default=""
+    )
 
     def __str__(self):
         return self.title
